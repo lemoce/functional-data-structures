8408d112cfee0c799a66b9bf035dfa59ce6a76a8
lemoce <cerencio@yahoo.com.br>
2018-01-04 11:43:57 -0200

    Start Chap 3

---
+++ src/Data/Lemoce/Chap3/Heap.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+module Data.Lemoce.Chap3.Heap
+  ( Heap (..)
+  , HeapTree (..))
+where
+
+import           Control.Exception
+
+data Empty = Empty deriving (Show, Eq)
+
+instance Exception Empty
+
+class (Ord a) => Heap t a where
+  empty :: t a
+  isEmpty :: t a -> Bool
+  insert :: a -> t a -> t a
+  merge :: t a -> t a -> t a
+  findMin :: t a -> a
+  deleteMin :: t a -> t a
+
+data HeapTree a = E | T Int a (HeapTree a) (HeapTree a)
+
+instance (Ord a) => Heap HeapTree a where
+  empty = E
+  isEmpty E = True
+  isEmpty _ = False
+
+  merge h E = h
+  merge E h = h
+  merge h1@(T _ x a1 b1) h2@(T _ y a2 b2) =
+       if x <= y
+          then makeT x a1 (merge b1 h2)
+          else makeT y a2 (merge b2 h1)
+    where
+      rank E           = 0
+      rank (T r _ _ _) = r
+
+      makeT x ha hb = if rank ha >= rank hb
+                        then T (rank hb + 1) x ha hb
+                        else T (rank ha + 1) x hb ha
+
+
+  insert x h = merge (T 1 x E E) h
+
+  findMin E           = throw Empty
+  findMin (T _ x _ _) = x
+
+  deleteMin E           = throw Empty
+  deleteMin (T _ x a b) = merge a b
